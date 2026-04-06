1aac6853a9068acb1826564c4ed15e6b70c87cff
ThoughtfulScholar <andrei.razvan.calin@gmail.com>
2026-04-06 14:44:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 #include <array>
 #include <chrono>
 #include <thread>
-
 #include <raylib-cpp.hpp>
-
-#include <iostream>
-#include <array>
 #include "include/Example.h"
 // This also works if you do not want `include/`, but some editors might not like it
 // #include "Example.h"
